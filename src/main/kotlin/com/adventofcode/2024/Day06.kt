b142e1d55e6ce837417f79ae340d8a216ca1ae35
vinayagasundar <vinayagsundar93@gmail.com>
2024-12-06 19:44:07 +0800

    day 6 solution part 2 and clean up

--- src/main/kotlin/com/adventofcode/2024/Day06.kt
+++ src/main/kotlin/com/adventofcode/2024/Day06.kt
@@ -30,39 +30,26 @@ enum class Direction(val x: Int, val y: Int) {
 
 fun main() {
     val inputFile = readFileFromResources("day06.txt").orEmpty()
-    var (x, y) = 0 to 0
-    var direction = Direction.Up
+    var (startX, startY) = 0 to 0
+    var startDir = Direction.Up
     val map = inputFile.lineSequence()
         .map { it.split("").toTypedArray() }
         .onEachIndexed { i, array ->
             array.onEachIndexed { j, c ->
                 Direction.getDirectionFromArrow(c)?.let { d ->
-                    x = i
-                    y = j
-                    direction = d
+                    startX = i
+                    startY = j
+                    startDir = d
                 }
             }
         }
         .toList()
-    var count = 1
-    val path = mutableListOf<String>()
-    val visited = mutableSetOf<String>()
-    visited.add("$x,$y")
-
+    var (x, y, direction) = Triple(startX, startY, startDir)
+    val seen = mutableSetOf<Pair<Int, Int>>()
+    seen.add(x to y)
     while (!map.isEdge(x, y, direction)) {
-        path.add("$x,$y,$direction")
         val (nx, ny) = x + direction.x to y + direction.y
         val next = map[nx][ny]
-//        if (next == "#") {
-//            direction = direction.turn90()
-//        } else if (next == "X" || next == ".") {
-//            if (next == ".") {
-//                count += 1
-//            }
-//            map[x][y] = "X"
-//            x = nx
-//            y = ny
-//        }
 
         if (next == "#") {
             direction = direction.turn90()
@@ -70,17 +57,61 @@ fun main() {
             x = nx
             y = ny
         }
+        seen.add(x to y)
+    }
+    println("Unique Points ${seen.size}")
 
-        if (visited.add("$x,$y")) {
-            count += 1
+    var counter = 0
+
+    for ((ox, oy) in seen) {
+        if (willLoop(map, startX, startY, startDir, ox, oy)) {
+            counter += 1
         }
     }
-//
-//    if (map[x][y] == ".") {
-//        count += 1
-//    }
 
-    println("Count $count")
+    println("Loop Counter $counter")
+}
+
+private fun willLoop(
+    map: List<Array<String>>,
+    startX: Int,
+    startY: Int,
+    direction: Direction,
+    ox: Int,
+    oy: Int
+): Boolean {
+    if (map[ox][oy] == "#")
+        return false
+
+    map[ox][oy] = "#"
+
+    var (x, y) = startX to startY
+    var d = direction
+
+    val seen = mutableSetOf<Triple<Int, Int, Direction>>()
+
+    while (true) {
+        if (map.isEdge(x, y, d)) {
+            map[ox][oy] = "."
+            return false
+        }
+
+        if (seen.add(Triple(x, y, d)).not()) {
+            map[ox][oy] = "."
+            return true
+        }
+
+        val (nx, ny) = x + d.x to y + d.y
+
+        val next = map[nx][ny]
+
+        if (next == "#") {
+            d = d.turn90()
+        } else {
+            x = nx
+            y = ny
+        }
+    }
 }
 
 fun List<Array<String>>.isEdge(x: Int, y: Int, direction: Direction): Boolean {
