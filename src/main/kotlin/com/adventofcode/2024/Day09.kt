f9ec635cf792dfb0123ffa9f964cfdd7735ffb2e
vinayagasundar <vinayagsundar93@gmail.com>
2024-12-09 15:17:19 +0800

    day 9 solution part 2

--- src/main/kotlin/com/adventofcode/2024/Day09.kt
+++ src/main/kotlin/com/adventofcode/2024/Day09.kt
@@ -15,6 +15,11 @@ fun main() {
             }
         }
 
+    println(partOne(file.clone(), space.clone()))
+    println(partTwo(file.clone(), space.clone()))
+}
+
+fun partOne(file: IntArray, space: IntArray): String {
     var i = 0
     var j = file.lastIndex
     val answer = mutableListOf<Int>()
@@ -41,5 +46,51 @@ fun main() {
         i++
     }
 
-    println(answer.map{ it.toLong()}.mapIndexed { i, v -> i * v  }.sum())
+    return answer.map { it.toLong() }.mapIndexed { i, v -> i * v }.sum().toString()
+}
+
+fun partTwo(file: IntArray, space: IntArray): String {
+    var i = 0
+    val answer = mutableListOf<Int>()
+    val fillerList = mutableMapOf<Int, Int>()
+
+    while (i < file.lastIndex) {
+        val fs = file[i]
+        for (f in 1..fs) {
+            answer.add(i)
+        }
+        file[i] = -1
+
+        val filler = fillerList.getOrDefault(i, -1)
+        if (filler > 0) {
+            for (f in 1..filler) {
+                answer.add(0)
+            }
+        }
+
+        var ss = space[i]
+
+        var j = file.lastIndex
+        while (j > i) {
+            if (file[j] in 1..ss) {
+                fillerList[j] = file[j]
+                for (f in 1..file[j]) {
+                    answer.add(j)
+                    ss--
+                }
+                file[j] = -1
+            } else {
+                j--
+            }
+        }
+
+        while (ss > 0) {
+            answer.add(0)
+            ss--
+        }
+
+        i++
+    }
+
+    return answer.map { it.toLong() }.mapIndexed { i, v -> i * v }.sum().toString()
 }
