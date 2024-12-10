4c937a873315050d0e9767b3909ded4e46cacf9b
vinayagasundar <vinayagasundar93@gmail.com>
2024-12-11 00:14:09 +0800

    2023 day 2 solution part 2

--- src/main/kotlin/com/adventofcode/yr2023/Day02.kt
+++ src/main/kotlin/com/adventofcode/yr2023/Day02.kt
@@ -2,18 +2,54 @@ package com.adventofcode.yr2023
 
 import com.adventofcode.readFileFromResources
 
-
+private const val RED_COLOR = "red"
+private const val GREEN_COLOR = "green"
+private const val BLUE_COLOR = "blue"
 
 fun main() {
-    fun String.getMax(): Int {
-        return when (this) {
-            "red" -> 12
-            "green" -> 13
-            else -> 14
-        }
-    }
+    println("${findAllValidGame()}")
+    println("${findMinimumCubePower()}")
+}
 
-    readFileFromResources("2023/day02.txt").orEmpty()
+private fun findMinimumCubePower(): Int {
+    return readFileFromResources("2023/day02.txt").orEmpty()
+        .lineSequence()
+        .mapIndexed { gameId, value ->
+            var maxRed = Int.MIN_VALUE
+            var maxGreen = Int.MIN_VALUE
+            var maxBlue = Int.MIN_VALUE
+             value.substringAfter(":")
+                .splitToSequence(";")
+                .filter { it.isNotEmpty() }
+                .forEach { rounds ->
+                    rounds.split(",")
+                        .filter { it.isNotEmpty() }
+                        .forEach { record ->
+                            val (count, color) = record.trim().split(" ")
+                            val countInt = count.toInt()
+
+                            if (color == RED_COLOR && maxRed < countInt) {
+                                maxRed = countInt
+                            }
+
+                            if (color == BLUE_COLOR && maxBlue < countInt) {
+                                maxBlue = countInt
+                            }
+
+                            if (color == GREEN_COLOR && maxGreen < countInt) {
+                                maxGreen = countInt
+                            }
+                        }
+                }
+
+            println("$maxRed $maxGreen $maxBlue")
+
+            maxRed * maxBlue * maxGreen
+        }.sum()
+}
+
+private fun findAllValidGame(): Int {
+    return readFileFromResources("2023/day02.txt").orEmpty()
         .lineSequence()
         .mapIndexed { gameId, value ->
             val valid = value.substringAfter(":")
@@ -35,7 +71,13 @@ fun main() {
             }
 
         }.sum()
-        .let {
-            println("$it")
-        }
+}
+
+private fun String.getMax(): Int {
+    return when (this) {
+        RED_COLOR -> 12
+        GREEN_COLOR -> 13
+        BLUE_COLOR -> 14
+        else -> throw IllegalArgumentException("Invalid color $this")
+    }
 }
