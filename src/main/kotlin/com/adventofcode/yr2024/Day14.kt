e9b0545ab2a1b2dbe2471ecdca2e35aeeef19273
vinayagasundar <vinayagasundar93@gmail.com>
2024-12-15 01:00:22 +0800

    day 14 solution part 2

--- src/main/kotlin/com/adventofcode/yr2024/Day14.kt
+++ src/main/kotlin/com/adventofcode/yr2024/Day14.kt
@@ -1,16 +1,18 @@
 package com.adventofcode.yr2024
 
-import com.adventofcode.readFileFromResources
+import com.adventofcode.*
+import java.io.BufferedWriter
+import java.io.FileWriter
 import kotlin.math.abs
 
 
 private const val MAX_ROW = 103
 private const val MAX_COLUMN = 101
 
-private fun displayMap(position: Array<Pair<Int, Int>>) {
+private fun displayMap(position: Array<Point>) {
     for (i in 0 until MAX_ROW) {
-        for (j in 0 until  MAX_COLUMN) {
-            val value = if (position.contains(i to j)) {
+        for (j in 0 until MAX_COLUMN) {
+            val value = if (position.contains(Point(i, j))) {
                 "#"
             } else {
                 "."
@@ -28,33 +30,33 @@ fun main() {
         .lines()
 
     val p = Array(input.size) {
-        0 to 0
+        Point(0, 0)
     }
     val v = Array(input.size) {
-        0 to 0
+        Point(0, 0)
     }
 
     input.forEachIndexed { i, line ->
         val (px, py, vx, vy) = regex.findAll(line).map { it.value.toInt() }.toList()
-        p[i] = py to px
-        v[i] = vy to vx
+        p[i] = Point(py, px)
+        v[i] = Point(vy, vx)
     }
 
     println("Part 1: ${solvePartOne(p.clone(), v.clone())}")
-    println("Part 1: ${solvePartOne(p.clone(), v.clone())}")
+    println("Part 2: ${solvePartTwo(p.clone(), v.clone())}")
 }
 
-private fun moveRobot(p: Array<Pair<Int, Int>>, v: Array<Pair<Int, Int>>) {
+private fun moveRobot(p: Array<Point>, v: Array<Point>) {
     for (i in p.indices) {
         val (px, py) = p[i]
         val (vx, vy) = v[i]
         val x = abs((px + vx + MAX_ROW) % MAX_ROW)
         val y = abs((py + vy + MAX_COLUMN) % MAX_COLUMN)
-        p[i] = x to y
+        p[i] = Point(x, y)
     }
 }
 
-private fun solvePartOne(position: Array<Pair<Int, Int>>, velocity: Array<Pair<Int, Int>>): Int {
+private fun solvePartOne(position: Array<Point>, velocity: Array<Point>): Int {
     for (i in 1..100) {
         moveRobot(position, velocity)
     }
@@ -79,4 +81,31 @@ private fun solvePartOne(position: Array<Pair<Int, Int>>, velocity: Array<Pair<I
     displayMap(position)
 
     return topLeft * topRight * bottomLeft * bottomRight
+}
+
+private fun solvePartTwo(position: Array<Point>, velocity: Array<Point>) {
+    val writer = BufferedWriter(FileWriter("output.txt"))
+
+    writer.use {
+        var c = 1
+        while (true) {
+            moveRobot(position, velocity)
+
+            writer.newLine()
+            writer.write("Counter : $c")
+            writer.newLine()
+            println("$c")
+            for (i in 0 until MAX_ROW) {
+                for (j in 0 until MAX_COLUMN) {
+                    if (position.contains(Point(i, j))) {
+                        writer.write("#")
+                    } else {
+                        writer.write(".")
+                    }
+                }
+                writer.newLine()
+            }
+            c++
+        }
+    }
 }
