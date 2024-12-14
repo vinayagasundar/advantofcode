9fa45c4f28e23b5da1279d233c81e3c8f984ba73
vinayagasundar <vinayagasundar93@gmail.com>
2024-12-15 00:10:43 +0800

    optimized the code to use same list

--- src/main/kotlin/com/adventofcode/yr2024/Day14.kt
+++ src/main/kotlin/com/adventofcode/yr2024/Day14.kt
@@ -1,65 +1,73 @@
 package com.adventofcode.yr2024
 
 import com.adventofcode.readFileFromResources
+import kotlin.math.abs
+
+
+private const val MAX_ROW = 103
+private const val MAX_COLUMN = 101
+
+private fun displayMap(position: Array<Pair<Int, Int>>) {
+    for (i in 0 until MAX_ROW) {
+        for (j in 0 until  MAX_COLUMN) {
+            val value = if (position.contains(i to j)) {
+                "#"
+            } else {
+                "."
+            }
+            print(value)
+        }
+        println()
+    }
+}
 
 fun main() {
     val regex = Regex("-*\\d+")
-    val maxRow = 103
-    val maxColumn = 101
-    val noOfSeconds = 100
+
     val input = readFileFromResources("day14.txt").orEmpty()
-        .lineSequence()
-        .map { line ->
-            val (px, py, vx, vy) = regex.findAll(line).map { it.value.toInt() }.toList()
-            var (cx, cy) = px to py
+        .lines()
 
-            for (i in 1..noOfSeconds) {
-                cx += vx
-                cy += vy
-
-                while (true) {
-                    if (cx in 0 until maxColumn && cy in 0 until maxRow) {
-                        break
-                    }
-
-                    if (cx < 0) {
-                        cx = maxColumn + cx
-                    }
-
-                    if (cx >= maxColumn) {
-                        cx = cx - maxColumn
-                    }
-
-                    if (cy < 0) {
-                        cy = maxRow + cy
-                    }
-
-                    if (cy >= maxRow) {
-                        cy = cy - maxRow
-                    }
-                }
-            }
-            (cy to cx)
-        }
-        .toList()
-
-
-    val midRow = maxRow / 2
-    val midColumn = maxColumn / 2
-
-    val map = Array(maxRow) {
-        IntArray(maxColumn) {
-            0
-        }
+    val p = Array(input.size) {
+        0 to 0
     }
+    val v = Array(input.size) {
+        0 to 0
+    }
+
+    input.forEachIndexed { i, line ->
+        val (px, py, vx, vy) = regex.findAll(line).map { it.value.toInt() }.toList()
+        p[i] = py to px
+        v[i] = vy to vx
+    }
+
+    println("Part 1: ${solvePartOne(p.clone(), v.clone())}")
+    println("Part 1: ${solvePartOne(p.clone(), v.clone())}")
+}
+
+private fun moveRobot(p: Array<Pair<Int, Int>>, v: Array<Pair<Int, Int>>) {
+    for (i in p.indices) {
+        val (px, py) = p[i]
+        val (vx, vy) = v[i]
+        val x = abs((px + vx + MAX_ROW) % MAX_ROW)
+        val y = abs((py + vy + MAX_COLUMN) % MAX_COLUMN)
+        p[i] = x to y
+    }
+}
+
+private fun solvePartOne(position: Array<Pair<Int, Int>>, velocity: Array<Pair<Int, Int>>): Int {
+    for (i in 1..100) {
+        moveRobot(position, velocity)
+    }
+
+    val midRow = MAX_ROW / 2
+    val midColumn = MAX_COLUMN / 2
 
     var topLeft = 0
     var topRight = 0
     var bottomLeft = 0
     var bottomRight = 0
 
-    for ((r, c) in input) {
-        map[r][c] += 1
+    for ((r, c) in position) {
         when {
             r < midRow && c < midColumn -> topLeft += 1
             r < midRow && c > midColumn -> topRight += 1
@@ -68,6 +76,7 @@ fun main() {
         }
     }
 
-    println(map.joinToString("\n") { it.joinToString(" ") })
-    println("Part ${topLeft * topRight * bottomLeft * bottomRight}")
+    displayMap(position)
+
+    return topLeft * topRight * bottomLeft * bottomRight
 }
